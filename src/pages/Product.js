7ac67d4bc593f889437054d0ca1858dca64c5f2f
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2022-02-17 12:53:11 +0200

    Update Product.js

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Button } from 'antd';
 
-const Product = () => (
+const Product = () => {
+  
+  return (
+  <>
   <div className="container">
     <Button type="primary">Product Page</Button>
   </div>
-);
+  </>
+)
+}
 
 export default Product;
