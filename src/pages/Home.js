2b35b0feb1ed998a3b61317c5b4d6dad8de0791c
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2022-02-17 12:50:53 +0200

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,15 @@ import { Link } from "react-router-dom";
 import Header from "../components/Header";
 import "./Home.css";
 
-const Home = () => (
+const Home = () => {
+
+return(
   <div className="container">
     <Header/>
      <Link to="/product" className="link">Product Page</Link>
      <Link to="/categories" className="link">Categories Page</Link>
   </div>
-);
+)
+}
 
 export default Home;
