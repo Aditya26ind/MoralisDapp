a629b0a6801b40d4a36f69498b7b49f395151990
IAmJaysWay <91806278+IAmJaysWay@users.noreply.github.com>
2022-02-17 12:48:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from './pages/Product';
 import Categories from './pages/Categories';
-import Checkout from './pages/Checkout';
 import './App.css';
 
 const App = () => (
@@ -11,7 +10,6 @@ const App = () => (
     <Route path="/" element={<Home />} />
     <Route path="product" element={<Product />} />
     <Route path="categories" element={<Categories />} />
-    <Route path="checkout" element={<Checkout />} />
   </Routes>
 );
 
